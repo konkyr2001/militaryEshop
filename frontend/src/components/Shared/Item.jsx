f2513958061b3599bc1141d19f30da5de3a287c2
konkyr2001 <konkyr2001@hotmail.com>
2026-03-16 23:15:36 +0200

    fixing image size

--- frontend/src/components/Shared/Item.jsx
+++ frontend/src/components/Shared/Item.jsx
@@ -164,7 +164,7 @@ function Item({
         )}
         {singlePost ? (
           <img
-            className="max-w-[250px] max-h-[250px] pointer"
+            className="max-w-[250px] max-h-[250px] h-[200px] w-[200px] bg-white pointer"
             // style={{ filter: "drop-shadow(5px 4px 1px rgba(0, 0, 0, 0.3))" }}
             src={icon}
           />
@@ -173,7 +173,7 @@ function Item({
             to={`/product/${id}`}
           >
             <img
-              className="max-w-[250px] max-h-[250px] pointer"
+              className="max-w-[250px] max-h-[250px] h-[200px] w-[200px] bg-white pointer"
               // style={{ filter: "drop-shadow(5px 4px 1px rgba(0, 0, 0, 0.3))" }}
               src={icon}
             />
